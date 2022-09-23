3f35e196d410b0bd26478e13b481ffb1e5547aad
akvetkovskij <akvetkovskij95@gmail.com>
2022-09-23 19:42:54 +0300

    Homework 3.4 task is ready

--- Homework_3/Homework3_4.py
+++ Homework_3/Homework3_4.py
@@ -1,2 +1,15 @@
 # Пользователь вводит 3 числа, сказать сколько из них положительных
 # и сколько отрицательных
+first_number = int(input('Enter your first number: '))
+second_number = int(input('Enter your second number: '))
+third_number = int(input('Enter your third number: '))
+comparison_1 = first_number >= 0
+comparison_2 = second_number >= 0
+comparison_3 = third_number >= 0
+positive_number = comparison_1 + comparison_2 + comparison_3
+negative_number = 3 - positive_number
+# print(comparison_1)
+# print(comparison_2)
+# print(comparison_3)
+print('Quantity of positive numbers is ' + str(positive_number))
+print('Quantity of negative numbers is ' + str(negative_number))
