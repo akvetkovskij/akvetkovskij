f7fb9343a5088479855f68ac1cff3351c7e9473e
akvetkovskij <akvetkovskij95@gmail.com>
2022-09-26 00:21:12 +0300

    Homework 4.2 is ready (but with internet help)

--- Homework_4/Homework4_2.py
+++ Homework_4/Homework4_2.py
@@ -1,9 +1,21 @@
 # Без использования collections, написать программу, которая будет
 # создавать словарь для подсчитывания количества вхождений каждой
 # буквы в текст введенный с клавиатуры
-text = 'hello'
-data = {i: i for i in text}
-print(data)
+text = input('Enter your text: ')
+text_before_list = set(list(text))
+second_dict = dict.fromkeys(text_before_list, 0)
+for text_before_list in text:
+    second_dict[text_before_list] += 1
+print(second_dict)
 
-numbers = [i for i in range(1, 100, 2)]
-print(numbers)
+
+
+
+# I dont know how to use this:
+
+# text = 'hello'
+# data = {i: i for i in text}
+# print(data)
+#
+# numbers = [i for i in range(1, 100, 2)]
+# print(numbers)
