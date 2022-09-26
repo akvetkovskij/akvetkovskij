967e4847f3780142089c5e7d41bba191519d6839
akvetkovskij <akvetkovskij95@gmail.com>
2022-09-26 12:01:53 +0300

    add first task in class

---
+++ Classroom/class.py
@@ -0,0 +1,8 @@
+# заполнить список четными числами в квадрате от 1 до N
+# n = int(input())
+# some_list = []
+# for i in range(n):
+#     if i % 2 == 0:
+#         some_list.append(i**2)
+# print(some_list)
+
