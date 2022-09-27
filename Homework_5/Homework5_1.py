19a3887120a3979523f08fb8768f526dd4a2a2bd
akvetkovskij <akvetkovskij95@gmail.com>
2022-09-28 02:49:36 +0300

    Homework5_1 task is ready

--- Homework_5/Homework5_1.py
+++ Homework_5/Homework5_1.py
@@ -1,4 +1,9 @@
 # Вывести первые N чисел кратные M и больше K
-counter = int(input('N '))
-splitter = int(input('M '))
-k_number = int(input('K '))
+counter = int(input('counter'))
+splitter = int(input('splitter'))
+k_number = int(input('K number'))
+number = 0
+for i in range(k_number, k_number*splitter):
+    if not i % splitter and number < counter:
+        print(i)
+        number += 1
