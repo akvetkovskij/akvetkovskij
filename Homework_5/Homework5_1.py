fe2c66d276f8f0089a66c486c318b28d998a2f0b
akvetkovskij <akvetkovskij95@gmail.com>
2022-09-28 03:23:08 +0300

    Homework5_3 some redactions

--- Homework_5/Homework5_1.py
+++ Homework_5/Homework5_1.py
@@ -3,7 +3,7 @@ counter = int(input('Enter count of numbers: '))
 splitter = int(input('Enter splitter: '))
 k_number = int(input('Enter minimal number: '))
 number = 0
-for i in range(k_number, k_number*splitter):
+for i in range(k_number, 1000000):
     if not i % splitter and number < counter:
         print(i)
         number += 1
