851e31377771da491e71008bb5851f2edea72b4f
akvetkovskij <akvetkovskij95@gmail.com>
2022-10-01 01:39:00 +0300

    Homework 6_1 is ready, but with help from internet

--- Homework_6/Homework6_1.py
+++ Homework_6/Homework6_1.py
@@ -6,10 +6,17 @@ def dec_in_bin(dvoich):
     return dvoich
 
 
-def bin_in_dec(desiatichnoe):
-    desiatichnoe = result
+def bin_in_dec(s):
+    number = 1
+    out = 0
+    for i in s[::-1]:
+        if i == '1':
+            out += number
+        number <<= 1
+    return out
 
 
-result = dec_in_bin(int(input()))
+result = dec_in_bin(int(input('Enter your number: ')))
 print(result)
-
+result_2 = bin_in_dec(result)
+print(result_2)
