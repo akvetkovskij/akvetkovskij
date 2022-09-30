594609e3bdb23ccd1113dd0b7cc91365dfcebe67
akvetkovskij <akvetkovskij95@gmail.com>
2022-09-30 03:34:32 +0300

    Homework6_1 first try to do this

--- Homework_6/Homework6_1.py
+++ Homework_6/Homework6_1.py
@@ -1,4 +1,15 @@
 # Написать функцию перевода десятичного числа в двоичное и обратно, без
 # использования функции int
-number = int(input())
-def perevod_v_dec()
+def dec_in_bin(dvoich):
+    dvoich = str(bin(dvoich))
+    dvoich = dvoich.removeprefix('0b')
+    return dvoich
+
+
+def bin_in_dec(desiatichnoe):
+    desiatichnoe = result
+
+
+result = dec_in_bin(int(input()))
+print(result)
+
