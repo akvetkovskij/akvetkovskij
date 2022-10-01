494e034b06faa7e1631beac9b71ac43e5bd1ff5a
akvetkovskij <akvetkovskij95@gmail.com>
2022-10-02 01:10:16 +0300

    Homework 6_3 task is ready

--- Homework_6/Homework6_3.py
+++ Homework_6/Homework6_3.py
@@ -1,2 +1,12 @@
 # Дан список чисел и на вход поступает число N, необходимо сместить список на
 # указанное число, пример: [1,2,3,4,5,6,7] N=3 ответ: [5,6,7,1,2,3,4]
+numbers = list(input('Enter your list: '))
+list_of_numbers = []
+for i in numbers:
+    if i.isdigit():
+        list_of_numbers.append(i)
+number = int(input('Enter your number shift: ')) + 1
+result_list = ()
+result_list = list_of_numbers[number:] + list_of_numbers[0:number]
+print(result_list)
+# print(list_of_numbers)
