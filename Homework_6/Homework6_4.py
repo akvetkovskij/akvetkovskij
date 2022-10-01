830184d352cf9ecd0c56b59a03493cf870994db8
akvetkovskij <akvetkovskij95@gmail.com>
2022-10-02 01:17:19 +0300

    Homework 6_4 task is ready

--- Homework_6/Homework6_4.py
+++ Homework_6/Homework6_4.py
@@ -1,3 +1,6 @@
 # Дан список содержащий в себе различные типы данных, отфильтровать таким
 # образом, чтобы остались только строки, использование дополнительного списка
 # незаконно
+list_with_data = input('Enter your list: ')
+list_with_data = list(filter(lambda i: i.isalpha(), list_with_data))
+print(list_with_data)
