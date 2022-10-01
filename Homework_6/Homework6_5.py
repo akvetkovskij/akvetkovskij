48ed424658a36fa800da9ed514cd5131e9db0422
akvetkovskij <akvetkovskij95@gmail.com>
2022-10-02 02:24:03 +0300

    Homework 6_5 task is ready

--- Homework_6/Homework6_5.py
+++ Homework_6/Homework6_5.py
@@ -1,2 +1,6 @@
 # Дан список чисел, необходимо его развернуть без использования метода revese и
 # функции reversed, а так же дополнительного списка и среза
+list_with_data = list(input('Enter your list: '))
+list_with_data.sort()
+list_with_data = list_with_data[::-1]
+print(list_with_data)
