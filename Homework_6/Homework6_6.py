36be642f829e9e56bb7bd0ad8cf732766b51ae65
akvetkovskij <akvetkovskij95@gmail.com>
2022-10-02 18:39:29 +0300

    Homework 6_6 task is ready

--- Homework_6/Homework6_6.py
+++ Homework_6/Homework6_6.py
@@ -1,2 +1,8 @@
 # Дан список рандомных чисел, необходимо отсортировать его в виде, сначала
 # четные, потом нечётные
+first_list = list(input('Enter your list: '))
+second_list = []
+result_list = []
+result_list = list(filter(lambda i: not int(i) % 2, first_list))
+result_list += list(filter(lambda i: int(i) % 2, first_list))
+print(result_list)
