f1e3d4c5e3c9d15192325879c7ca276f43cfe41b
akvetkovskij <akvetkovskij95@gmail.com>
2022-10-02 18:24:54 +0300

    Homework 6_5 without slice

--- Homework_6/Homework6_5.py
+++ Homework_6/Homework6_5.py
@@ -1,6 +1,11 @@
 # Дан список чисел, необходимо его развернуть без использования метода revese и
 # функции reversed, а так же дополнительного списка и среза
 list_with_data = list(input('Enter your list: '))
-# list_with_data.sort()
-list_with_data = list_with_data[::-1]
+number = 0
+while number < len(list_with_data)/2:
+    list_with_data[number], list_with_data[~number] = list_with_data[~number], list_with_data[number]
+    number += 1
 print(list_with_data)
+# list_with_data.sort()
+# list_with_data = list_with_data[::-1]
+# print(list_with_data)
