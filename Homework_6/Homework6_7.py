7ae298c5937a85ea3101435f136bc7209fbf43a0
akvetkovskij <akvetkovskij95@gmail.com>
2022-10-03 23:09:32 +0300

    Class

--- Homework_6/Homework6_7.py
+++ Homework_6/Homework6_7.py
@@ -1,11 +1,9 @@
 # Дан список чисел, необходимо для каждого элемента посчитать сумму его
 # соседей, для крайних чисел одним из соседей является число с противоположной
 # стороны списка
-first_list = [1, 2, 3, 4, 5, 6, 7]
-number = list(map(sum(first_list), first_list))
-
-# while number < len(first_list):
-#     print(first_list[number - 1] + first_list[number + 1])
-#     number += 1
-# else:
-#     print(first_list[0] + first_list[-1])
+input_list = [1, 2, 3, 4, 5, 6, 7]
+for i in range(len(input_list)):
+    if i != len(input_list) - 1:
+        print(input_list[i-1] + input_list[i+1])
+    else:
+        print(input_list[i-1] + input_list[0])
