b27aa9862540ee56c4c5f0b64be588da0d93294b
akvetkovskij <akvetkovskij95@gmail.com>
2022-10-04 01:13:30 +0300

    Homework6_8 first trying to do this

--- Homework_6/Homework6_8.py
+++ Homework_6/Homework6_8.py
@@ -1,2 +1,7 @@
 # Дан словарь, ключ - Название страны, значение - список городов, на вход
 # поступает город, необходимо сказать из какой он страны
+def search(city):
+    dict_with_countries = {
+        'Belarus': ['Minsk', 'Brest', 'Mogilev', 'Gomel', 'Vitebsk'],
+        'Austria': ['Vena', 'Graz', 'Linz', 'Salzburg', 'Insbruk']
+    }
