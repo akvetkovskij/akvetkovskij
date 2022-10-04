e5d52221e40646803d0ee4e77e7aa1a46307d320
akvetkovskij <akvetkovskij95@gmail.com>
2022-10-04 18:14:01 +0300

    Homework6_8 trying to do this

--- Homework_6/Homework6_8.py
+++ Homework_6/Homework6_8.py
@@ -1,7 +1,15 @@
 # Дан словарь, ключ - Название страны, значение - список городов, на вход
 # поступает город, необходимо сказать из какой он страны
-def search(city):
-    dict_with_countries = {
-        'Belarus': ['Minsk', 'Brest', 'Mogilev', 'Gomel', 'Vitebsk'],
-        'Austria': ['Vena', 'Graz', 'Linz', 'Salzburg', 'Insbruk']
-    }
+# def search(city):
+dict_with_countries = {
+    'Belarus': ('Minsk', 'Brest', 'Mogilev', 'Gomel', 'Vitebsk'),
+    'Austria': ('Vena', 'Graz', 'Linz', 'Salzburg', 'Insbruk')
+}
+first_list = ('Minsk', 'Brest', 'Mogilev', 'Gomel', 'Vitebsk')
+# print(dict_with_countries.values())
+# if 'Minsk' in first_list:
+#     print('go')
+# print(dict_with_countries.items())
+for countries in dict_with_countries.keys():
+    if 'Minsk' in dict_with_countries.values():
+        print(dict_with_countries.keys(countries))
