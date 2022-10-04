52e87ba5524791559a9d569571ce5853be5d6801
akvetkovskij <akvetkovskij95@gmail.com>
2022-10-05 01:34:27 +0300

    Homework6_8 task is ready

--- Homework_6/Homework6_8.py
+++ Homework_6/Homework6_8.py
@@ -1,15 +1,16 @@
 # Дан словарь, ключ - Название страны, значение - список городов, на вход
 # поступает город, необходимо сказать из какой он страны
-# def search(city):
-dict_with_countries = {
-    'Belarus': ('Minsk', 'Brest', 'Mogilev', 'Gomel', 'Vitebsk'),
-    'Austria': ('Vena', 'Graz', 'Linz', 'Salzburg', 'Insbruk')
-}
-first_list = ('Minsk', 'Brest', 'Mogilev', 'Gomel', 'Vitebsk')
-# print(dict_with_countries.values())
-# if 'Minsk' in first_list:
-#     print('go')
-# print(dict_with_countries.items())
-for countries in dict_with_countries.keys():
-    if 'Minsk' in dict_with_countries.values():
-        print(dict_with_countries.keys(countries))
+def search(city):
+    dict_with_countries = {
+        'Belarus': ('Minsk', 'Brest', 'Mogilev', 'Gomel', 'Vitebsk'),
+        'Austria': ('Vena', 'Graz', 'Linz', 'Salzburg', 'Insbruk'),
+        'USA': ('New York', 'Chicago', 'Houston', 'Phoenix', 'Columbus'),
+        'England': ('London', 'Birmingham', 'Leeds', 'Liverpool', 'Newcastle')
+    }
+    for i in dict_with_countries.keys():
+        if city in dict_with_countries.get(i):
+            return i
+
+
+result = search(input('Enter your city: '))
+print(f'Your city in the {result} contry')
