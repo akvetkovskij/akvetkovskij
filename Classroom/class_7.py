86beb93cc0b436171294fd45b90d581e9a94ae39
akvetkovskij <akvetkovskij95@gmail.com>
2022-10-07 02:11:27 +0300

    class

--- Classroom/class_7.py
+++ Classroom/class_7.py
@@ -1,6 +1,14 @@
 # Classwork 7
 # Вводится химическая формула: H2O -> {'H': 2, 'O': 1}
 # C2H5OH -> {'C': 2, 'H': 6, 'O': 1}
-formula = {}
-input_formula = 'C2H5OH'
-def
+# formula = {}
+# input_formula = 'C2H5OH'
+class Person:
+
+    def __init__(self, name: str, age: int):
+        self.name = name.title()
+        self.age = age
+
+
+vasya = Person('Vasya', 27)
+print(vasya.age)
