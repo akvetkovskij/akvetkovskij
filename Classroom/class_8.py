fb3f5404e0633d013175b17e27d9aa243bfe7d65
akvetkovskij <akvetkovskij95@gmail.com>
2022-10-17 13:21:42 +0300

    trying to do calendar

--- Classroom/class_8.py
+++ Classroom/class_8.py
@@ -5,3 +5,12 @@
 # на вход конструктора 'category', 'all', '0'
 # и возвращать user:category:all:0
 # Реализовать
+from datetime import datetime, timedelta
+
+data = datetime.now()
+dataplus = []
+delta = timedelta(minutes=30)
+plus = data + timedelta(minutes=30)
+datastr = plus.strftime("%d.%m.%Y %H:%M:%S")
+dataplus.append(datastr)
+print(dataplus)
--- Homework_8/Homework_8.py
+++ Homework_8/Homework_8.py
@@ -1,21 +1,24 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class Time:
     start_time: str
     end_time: str
-    delta: str
+    # delta: str
 
-    def __init__(self, start_time, end_time, delta):
+    def __init__(self, start_time, end_time):
         self.start_time = start_time
         self.end_time = end_time
-        self.delta = datetime.timedelta(minutes=30)
+        self.delta = timedelta(minutes=30)
 
     def get_timeline(self):
         timeline = []
         graph = 0
         while graph < 18:
-            timeline.append(self.start_time + self.delta)
+            date_plus_delta = self.start_time + self.delta
+            data_to_str = date_plus_delta.strftime("%d.%m.%Y %H:%M:%S")
+            timeline.append(data_to_str)
+            print(timeline)
             graph += 1
 
     def reserve_time(self):
