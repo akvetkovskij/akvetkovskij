fb26529a4da4285865f0cccac1ef195b64f64205
akvetkovskij <akvetkovskij95@gmail.com>
2022-10-20 02:21:40 +0300

    trying to do calendar

--- Homework_8/Homework_8.py
+++ Homework_8/Homework_8.py
@@ -6,24 +6,27 @@ class Time:
     # end_time: str
     # delta: str
 
-    def __init__(self, start_time):  # end_time
+    def __init__(self, start_time, end_time):  # end_time
         self.start_time = start_time
-        # self.end_time = end_time
+        self.end_time = end_time
         self.delta = timedelta(minutes=30)
 
-    def get_timeline(self, start_time):
+    def get_timeline(self):
         timeline = []
-        graph = 0
+        # graph = 0
+        end_time = datetime.strptime(self.end_time, "%d.%m.%Y %H:%M")
         start_time = datetime.strptime(self.start_time, "%d.%m.%Y %H:%M")
-        while graph < 18:
-            date_plus_delta = start_time + self.delta
-            data_to_str = date_plus_delta.strftime("%d.%m.%Y %H:%M")
-            timeline.append(data_to_str)
-            graph += 1
-        print(timeline)
+        while start_time < end_time:
+            timeline.append(start_time)
+            start_time += self.delta
+            # date_plus_delta = start_time + self.delta
+            # data_to_str = date_plus_delta.strftime("%d.%m.%Y %H:%M")
+            # timeline.append(data_to_str)
+            # graph += 1
+        return timeline
 
     def reserve_time(self):
         pass
 
 
-Time.get_timeline("17.10.2022 10:00")
+print(Time("17.10.2022 10:00").get_timeline())
