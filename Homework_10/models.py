6ee6f57a788b4212623eda232a91967c6828bfea
akvetkovskij <akvetkovskij95@gmail.com>
2022-10-19 23:04:08 +0300

    add alchemy on Python

---
+++ Homework_10/models.py
@@ -0,0 +1,54 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, VARCHAR, ForeignKey, BOOLEAN
+
+
+Base = declarative_base()
+
+
+class Category(Base):
+    __tablename__: str = 'categories'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(VARCHAR(24), nullable=False, unique=True)    # nullable=False - не может быть пустым, если True - то может
+
+
+class Product(Base):
+    __tablename__: str = 'products'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(VARCHAR(36), nullable=False, unique=True)
+    descr = Column(VARCHAR(140))
+    category_id = Column(Integer, ForeignKey('categories.id', ondelete='CASCADE'), nullable=False)
+
+
+class User(Base):
+    __tablename__: str = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(VARCHAR(24), nullable=False)
+    email = Column(VARCHAR(24), nullable=False, unique=True)
+
+
+class Status(Base):
+    __tablename__: str = 'statuses'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(VARCHAR(10), nullable=False, unique=True)
+
+
+class Order(Base):
+    __tablename__: str = 'orders'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    status_id = Column(Integer, ForeignKey('statuses.id', ondelete='CASCADE'), nullable=False)
+
+
+class OrderItem(Base):
+    __tablename__: str = 'order_items'
+
+    id = Column(Integer, primary_key=True)
+    order_id = Column(Integer, ForeignKey('orders.id', ondelete='CASCADE'), nullable=False)
+    product_id = Column(Integer, ForeignKey('products.id', ondelete='CASCADE'), nullable=False)
+
+
