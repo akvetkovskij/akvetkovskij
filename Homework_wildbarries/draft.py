1a2de5419fbfad78614358e2561d77d5ce2ff337
akvetkovskij <akvetkovskij95@gmail.com>
2022-10-26 00:02:11 +0300

    classwork + homework

--- Homework_wildbarries/draft.py
+++ Homework_wildbarries/draft.py
@@ -23,58 +23,62 @@ def create_session(func):
     return wrapper
 
 
-@create_session
-def add_category(category: Category, session=None) -> Optional[Category]:
-    """
-    For adding some information in database
-    :param session: it is decorator
-    :param category: name
-    :return: Category
-    """
-    session.add(category)
-    try:
-        session.commit()
-    except IntegrityError:
-        return None
-    else:
-        session.refresh(category)
-        return category
+class CRUDCategory(object):
 
+    @staticmethod
+    @create_session
+    def add(category: Category, session=None) -> Optional[Category]:
+        """
+        For adding some information in database
+        :param session: it is decorator
+        :param category: name
+        :return: Category
+        """
+        session.add(category)
+        try:
+            session.commit()
+        except IntegrityError:
+            return None
+        else:
+            session.refresh(category)
+            return category
 
-@create_session
-def get_category(category_id: int, session=None) -> Optional[Category]:
-    """
-    For getting information about category with id
-    :param session: it is decorator
-    :param category_id: id
-    :return: Category
-    """
-    category = session.execute(
-        select(Category).where(Category.id == category_id)
-    )
-    category = category.first()
-    if category:
-        return category[0]
+    @staticmethod
+    @create_session
+    def get(category_id: int, session=None) -> Optional[Category]:
+        """
+        For getting information about category with id
+        :param session: it is decorator
+        :param category_id: id
+        :return: Category
+        """
+        category = session.execute(
+            select(Category).where(Category.id == category_id)
+        )
+        category = category.first()
+        if category:
+            return category[0]
 
-
-def all_categories() -> List[Category]:
-    with Session() as session:
+    @staticmethod
+    @create_session
+    def all(session=None) -> List[Category]:
         categories = session.execute(
             select(Category)
             .order_by(Category.id)
         )
         return [category[0] for category in categories]  # для распаковки
 
-
-def update_category(category: Category) -> bool:
-    """
-    This function for update information in table 'Category'
-    :param category: Catgory.id???
-    :return: Categoru.id before update
-    """
-    category = category.__dict__
-    del category['_sa_instance_state']
-    with Session() as session:
+    @staticmethod
+    @create_session
+    def update(category: Category, session=None) -> bool:
+        """
+        This function for update information in table 'Category'
+        :param session: It is a decorator
+        :param category: Catgory.id???
+        :return: Categoru.id before update
+        """
+        category = category.__dict__
+        del category['_sa_instance_state']
         session.execute(
             update(Category)
             .where(Category.id == category.get('id'))
@@ -82,24 +86,24 @@ def update_category(category: Category) -> bool:
                 **category
             )
         )
-    try:
+        try:
+            session.commit()
+        except IntegrityError:
+            return False
+        else:
+            return True
+
+    @staticmethod
+    @create_session
+    def delete(category_id: int, session=None) -> None:
+        """
+        For deleting Category
+        :param session: it is decorator
+        :param category_id: id category for delete
+        :return: Nothing
+        """
+        session.execute(
+            delete(Category)
+            .where(Category.id == category_id)
+        )
         session.commit()
-    except IntegrityError:
-        return False
-    else:
-        return True
-
-
-@create_session
-def delete_category(category_id: int, session=None) -> None:
-    """
-    For deleting Category
-    :param session: it is decorator
-    :param category_id: id category for delete
-    :return: Nothing
-    """
-    session.execute(
-        delete(Category)
-        .where(Category.id == category_id)
-    )
-    session.commit()
