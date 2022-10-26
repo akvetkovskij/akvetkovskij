b0b4e19689a9029e50ce613574d33eff82cb6550
akvetkovskij <akvetkovskij95@gmail.com>
2022-10-26 20:43:32 +0300

    classwork + homework

--- alembic/env.py
+++ alembic/env.py
@@ -1,12 +1,12 @@
+from Homework_10.settings import DATABASE_URL
+from Homework_10.models import Base
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-
-from Homework_10.settings import DATABASE_URL
-from Homework_10.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
