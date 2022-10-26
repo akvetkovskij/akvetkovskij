e84602c5d4b1dbf7a2ce4f8af8a60f204c478d57
akvetkovskij <akvetkovskij95@gmail.com>
2022-10-26 16:31:36 +0300

    classwork + homework

--- Homework_10/models.py
+++ Homework_10/models.py
@@ -10,6 +10,7 @@ class Category(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(VARCHAR(24), nullable=False, unique=True)    # nullable=False - не может быть пустым, если True - то может
+    descr = Column(VARCHAR(140))
 
 
 class Product(Base):
