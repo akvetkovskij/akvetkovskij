ff8da3ca2c134a25a32acd4f1bb8c69db9f43231
akvetkovskij <akvetkovskij95@gmail.com>
2022-10-28 15:36:14 +0300

    add CRUDOrder in package

--- crud/__init__.py
+++ crud/__init__.py
@@ -1,6 +1,10 @@
 from .category import CRUDCategory
+from .orders import CRUDOrder
+from .products import CRUDProduct
 
 
 __all__ = [
     'CRUDCategory',
+    'CRUDOrder',
+    'CRUDProduct',
 ]
---
+++ crud/products.py
@@ -0,0 +1,61 @@
+from typing import Optional, List
+
+from sqlalchemy import select, update, delete
+from sqlalchemy.exc import IntegrityError
+
+from Homework_10.models import create_session, Product
+
+
+class CRUDProduct(object):
+
+    @staticmethod
+    @create_session
+    def add(product: Product, session=None):
+        session.add(product)
+        try:
+            session.commit()
+        except IntegrityError:
+            return None
+        else:
+            session.refresh(product)
+            return product
+
+    @staticmethod
+    @create_session
+    def get(product_id: int, session=None) -> Optional[Product]:
+        product = session.execute(
+            select(Product)
+            .where(Product.id == product_id)
+        )
+        product = product.first()
+        if product:
+            return product[0]
+
+    @staticmethod
+    @create_session
+    def all(session=None) -> List[Product]:
+        products = session.execute(
+            select(Product)
+            .order_by(Product.id)
+        )
+        return [product[0] for product in products]
+
+    @staticmethod
+    @create_session
+    def update(product: Product, session=None) -> bool:
+        product = product.__dict__
+        del product['_sa_instance_state']
+        session.execute(
+            update(Product)
+            .where(Product.id == product.get('id'))
+            .values(
+                **product
+            )
+        )
+        try:
+            session.commit()
+        except IntegrityError:
+            return False
+        else:
+            return True
+
