586c2dbd95ab072d687feb43894da0ff19e6d97b
akvetkovskij <akvetkovskij95@gmail.com>
2022-10-28 18:12:22 +0300

    add CRUDUser in package

--- crud/__init__.py
+++ crud/__init__.py
@@ -1,10 +1,12 @@
 from .category import CRUDCategory
 from .orders import CRUDOrder
 from .products import CRUDProduct
+from .user import CRUDUser
 
 
 __all__ = [
     'CRUDCategory',
     'CRUDOrder',
     'CRUDProduct',
+    'CRUDUser',
 ]
