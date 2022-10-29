2a0f60b3b0c043d4468b693c6e6d2fb46324577a
akvetkovskij <akvetkovskij95@gmail.com>
2022-10-29 18:19:10 +0300

    editing init

--- crud/__init__.py
+++ crud/__init__.py
@@ -2,6 +2,8 @@ from .category import CRUDCategory
 from .orders import CRUDOrder
 from .products import CRUDProduct
 from .user import CRUDUser
+from .status import CRUDStatus
+from .order_item import CRUDOrderItem
 
 
 __all__ = [
@@ -9,4 +11,6 @@ __all__ = [
     'CRUDOrder',
     'CRUDProduct',
     'CRUDUser',
+    'CRUDStatus',
+    'CRUDOrderItem',
 ]
