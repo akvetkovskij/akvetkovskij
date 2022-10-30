19595eeb976eff20351e8532ebb842333bed1b65
akvetkovskij <akvetkovskij95@gmail.com>
2022-10-31 00:33:04 +0300

    editing JSON Wildberries

--- Homework_wildbarries/JSON_Wildberies.py
+++ Homework_wildbarries/JSON_Wildberies.py
@@ -1,2 +1,12 @@
-import sqlite3
+from requests import Session
 
+
+def get_response():
+    with Session() as session:
+        response = session.get(
+            url='https://www.wildberries.by/webapi/menu/main-menu-by-ru.json'
+        )
+        print(response.json())
+
+
+print(get_response())
