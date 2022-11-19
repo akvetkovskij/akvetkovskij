308f5a09238b01f3a3d41175aa89130a40150fca
akvetkovskij <akvetkovskij95@gmail.com>
2022-11-19 03:53:21 +0300

    editing JSON Wildberries as async

--- crud/category.py
+++ crud/category.py
@@ -2,15 +2,18 @@ from typing import Optional, List
 
 from sqlalchemy import select, update, delete
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from Homework_10.models import create_session, Category
+# from Homework_10.models import create_session, Category
+from Homework_10.models import create_async_session, Category
 
 
 class CRUDCategory(object):
 
     @staticmethod
-    @create_session
-    def add(category: Category, session=None) -> Optional[Category]:
+    # @create_session
+    @create_async_session
+    async def add(category: Category, session: AsyncSession = None) -> Optional[Category]:
         """
         For adding some information in database
         :param session: it is decorator
@@ -19,23 +22,24 @@ class CRUDCategory(object):
         """
         session.add(category)
         try:
-            session.commit()
+            await session.commit()
         except IntegrityError:
             return None
         else:
-            session.refresh(category)
+            await session.refresh(category)
             return category
 
     @staticmethod
-    @create_session
-    def get(category_id: int, session=None) -> Optional[Category]:
+    # @create_session
+    @create_async_session
+    async def get(category_id: int, session: AsyncSession = None) -> Optional[Category]:
         """
         For getting information about category with id
         :param session: it is decorator
         :param category_id: id
         :return: Category
         """
-        category = session.execute(
+        category = await session.execute(
             select(Category).where(Category.id == category_id)
         )
         category = category.first()
@@ -43,17 +47,19 @@ class CRUDCategory(object):
             return category[0]
 
     @staticmethod
-    @create_session
-    def all(session=None) -> List[Category]:
-        categories = session.execute(
+    # @create_session
+    @create_async_session
+    async def all(session: AsyncSession = None) -> List[Category]:
+        categories = await session.execute(
             select(Category)
             .order_by(Category.id)
         )
         return [category[0] for category in categories]  # для распаковки
 
     @staticmethod
-    @create_session
-    def update(category: Category, session=None) -> bool:
+    # @create_session
+    @create_async_session
+    async def update(category: Category, session: AsyncSession = None) -> bool:
         """
         This function for update information in table 'Category'
         :param session: It is a decorator
@@ -62,7 +68,7 @@ class CRUDCategory(object):
         """
         category = category.__dict__
         del category['_sa_instance_state']
-        session.execute(
+        await session.execute(
             update(Category)
             .where(Category.id == category.get('id'))
             .values(
@@ -70,26 +76,27 @@ class CRUDCategory(object):
             )
         )
         try:
-            session.commit()
+            await session.commit()
         except IntegrityError:
             return False
         else:
             return True
 
     @staticmethod
-    @create_session
-    def delete(category_id: int, session=None) -> None:
+    # @create_session
+    @create_async_session
+    async def delete(category_id: int, session: AsyncSession = None) -> None:
         """
         For deleting Category
         :param session: it is decorator
         :param category_id: id category for delete
         :return: Nothing
         """
-        session.execute(
+        await session.execute(
             delete(Category)
             .where(Category.id == category_id)
         )
-        session.commit()
+        await session.commit()
 
 
 # CRUDCategory.delete(category_id=1)
